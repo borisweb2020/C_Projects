c35c96b195146f6de55ef2d9ce4756cb65cdce51
borisweb2020 <49887284+borisweb2020@users.noreply.github.com>
2023-03-19 16:39:59 +0700

    Format the code with clang-format

--- 1_Intro/helloWorld.c
+++ 1_Intro/helloWorld.c
@@ -2,20 +2,13 @@
 
 int main() {
     char nickname[10];
-    scanf("%9[A-Za-z]s", nickname); // input aks123 --- output aks
-    scanf("%9[^0123456789\n]s", nickname); // input aks123 --- output aks
+    scanf("%9[A-Za-z]s", nickname);         // input aks123 --- output aks
+    scanf("%9[^0123456789\n]s", nickname);  // input aks123 --- output aks
     if (!scanf("%9[A-Za-z]s", nickname)) {
-      printf("Not valid character\n");
+        printf("Not valid character\n");
     } else {
-      printf("Hello, %s\n", nickname);
+        printf("Hello, %s\n", nickname);
     }
-
-    for (int i = 0; i < 10; i++) {
-
-    }
-
-    
-    
 
     return 0;
 }
