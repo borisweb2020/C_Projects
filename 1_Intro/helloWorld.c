12990354fb3478d84ba26c160980652e7485f451
borisweb2020 <49887284+borisweb2020@users.noreply.github.com>
2023-03-19 15:57:29 +0700

    Ad a check for the input data

--- 1_Intro/helloWorld.c
+++ 1_Intro/helloWorld.c
@@ -1,5 +1,21 @@
 #include <stdio.h>
 
-int main(void) {
-	printf("Hello, World!");
+int main() {
+    char nickname[10];
+    scanf("%9[A-Za-z]s", nickname); // input aks123 --- output aks
+    scanf("%9[^0123456789\n]s", nickname); // input aks123 --- output aks
+    if (!scanf("%9[A-Za-z]s", nickname)) {
+      printf("Not valid character\n");
+    } else {
+      printf("Hello, %s\n", nickname);
+    }
+
+    for (int i = 0; i < 10; i++) {
+
+    }
+
+    
+    
+
+    return 0;
 }
