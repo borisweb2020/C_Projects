b09d1bc2eac0d7db41180f97ca9733245b7a1a54
borisweb2020 <49887284+borisweb2020@users.noreply.github.com>
2023-04-02 18:32:15 +0700

    Format with clang-format

--- 2_Basic_control_structures/random.c
+++ 2_Basic_control_structures/random.c
@@ -3,31 +3,31 @@
 #include <time.h>
 
 int main() {
-    int rand_1 = RAND_MAX; // наибольшее возможное значение, которое может сгенерировать функция rand()
+    int rand_1 = RAND_MAX;  // наибольшее возможное значение, которое может сгенерировать функция rand()
     printf("RAND_MAX: %d\n", rand_1);
 
-    int rand_2 = rand(); // сгенерирует случайное число от 0 до RAND_MAX
+    int rand_2 = rand();  // сгенерирует случайное число от 0 до RAND_MAX
     printf("Random value: %d\n", rand_2);
 
-    int rand_3 = rand() % 15; // сгенерирует случайное число от 0 до 14
+    int rand_3 = rand() % 15;  // сгенерирует случайное число от 0 до 14
     printf("Random value: %d\n", rand_3);
 
-    printf("Number of seconds: %ld\n", time(NULL)); // количество секунд с начала эпохи 01.01.1970
+    printf("Number of seconds: %ld\n", time(NULL));  // количество секунд с начала эпохи 01.01.1970
 
     srand(time(NULL));  // инициализирует генератор случайных чисел с заданным начальным значением
 
-    int rand_4 = rand() % 15; // сгенерирует случайное число от 0 до 14
+    int rand_4 = rand() % 15;  // сгенерирует случайное число от 0 до 14
     printf("Random value from 0 to 14: %d\n", rand_4);
 
     float rand_5;
-    rand_5 = ((float)rand() / RAND_MAX) * 15.4; // генерирует случайное вещественное число от 0,0 до 15,4
+    rand_5 = ((float)rand() / RAND_MAX) * 15.4;  // генерирует случайное вещественное число от 0,0 до 15,4
     printf("Random value from 0.0 to 15.4: %.2f\n", rand_5);
 
     /* Генерация случайного вещественного числа в диапазоне от -10,0 до 15,4 */
     float rand_6, lower_bound, upper_bound;
     lower_bound = -10.0;
     upper_bound = 15.4;
-    rand_6 = ((float) rand() / RAND_MAX) * (upper_bound - lower_bound) + lower_bound;
+    rand_6 = ((float)rand() / RAND_MAX) * (upper_bound - lower_bound) + lower_bound;
     printf("Random value [-10.0; 15.4]: %.3f\n", rand_6);
 
     /* Генерация случайного целого числа в диапазоне от -10 до 10 */
