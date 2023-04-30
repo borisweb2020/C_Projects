6a2119c314e158c2fe08f905b5993feb9fe6d2fa
borisweb2020 <49887284+borisweb2020@users.noreply.github.com>
2023-04-30 15:58:21 +0700

    Format with .clang-format

--- 3_Strings_Functions/strlen.c
+++ 3_Strings_Functions/strlen.c
@@ -3,21 +3,21 @@
 
 int main() {
     char name[] = "Prodijy";
-    printf("A length of the string %s is equal %lu\n", name, strlen(name)); // length == 7
+    printf("A length of the string %s is equal %lu\n", name, strlen(name));  // length == 7
 
-    char name_2[10] = "Prodijy\n"; // Если получаем через scanf() или fgets(), то
-    printf("A length of the string %s is equal %lu\n", name_2, strlen(name_2)); // length == 8
+    char name_2[10] = "Prodijy\n";  // Если получаем через scanf() или fgets(), то
+    printf("A length of the string %s is equal %lu\n", name_2, strlen(name_2));  // length == 8
     if (name_2[strlen(name_2) - 1] == '\n') {
-        name_2[strlen(name_2) - 1] = '\0'; // поэтому удаляем символ \n
+        name_2[strlen(name_2) - 1] = '\0';  // поэтому удаляем символ \n
     }
-    printf("A length of the string %s is equal %lu\n", name_2, strlen(name_2)); // length == 7
+    printf("A length of the string %s is equal %lu\n", name_2, strlen(name_2));  // length == 7
 
     char name_3[10];
     char c;
-    if (fgets(name_3, sizeof(name_3), stdin)) { // Если будет введено более 9 символов
+    if (fgets(name_3, sizeof(name_3), stdin)) {  // Если будет введено более 9 символов
         printf("A length of the string %s is equal %lu\n", name_3, strlen(name_3));
     }
-    c = getchar(); // то 10-й будет перехвачен здесь. Поэтому нужно чистить буфер 
+    c = getchar();  // то 10-й будет перехвачен здесь. Поэтому нужно чистить буфер
     putchar(c);
 
     return 0;
