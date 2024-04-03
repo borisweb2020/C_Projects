5d22a098a2d93e037490642c57605a9d85f45b05
borisweb2020 <49887284+borisweb2020@users.noreply.github.com>
2024-04-03 16:39:32 +0700

    Fix the code to consider zero division and explicit type conversion

--- 1_Intro/arithmetic.c
+++ 1_Intro/arithmetic.c
@@ -1,20 +1,28 @@
 #include <stdio.h>
 
+int add(int a, int b);
+int subtract(int a, int b);
+int multiply(int a, int b);
+float divide(int a, int b);
+
 int main() {
     int a, b;
     char c;
+
     if (scanf("%d%d%c", &a, &b, &c) == 3 && c == '\n') {
-        printf("The sum is %d\n", a + b);
-        printf("The difference is %d\n", a - b);
-        printf("The product is %d\n", a * b);
         if (b != 0) {
-            printf("The quotient is %d\n", a / b);
+            printf("%d %d %d %.1f", add(a, b), subtract(a, b), multiply(a, b), divide(a, b));
         } else {
-            printf("Division by zero\n");
+            printf("%d %d %d n/a", add(a, b), subtract(a, b), multiply(a, b));
         }
     } else {
-        printf("n/a\n");
+        printf("n/a");
     }
 
     return 0;
 }
+
+int add(int a, int b) { return a + b; }
+int subtract(int a, int b) { return a - b; }
+int multiply(int a, int b) { return a * b; }
+float divide(int a, int b) { return (float)a / b; }
