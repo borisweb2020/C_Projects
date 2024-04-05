0723d6c433cd8b93632f3cf7a74b2beb97996490
borisweb2020 <49887284+borisweb2020@users.noreply.github.com>
2024-04-05 17:27:13 +0700

    Perform the encoding/decoding task

--- 2_Basic_control_structures/char_decode.c
+++ 2_Basic_control_structures/char_decode.c
@@ -1,8 +1,27 @@
 #include <stdio.h>
+
 void encode();
 void decode();
 
 int main(int argc, char *argv[]) {
+    char c = 76;
+    printf("%d\n", c);  // 76
+    printf("%c\n", c);  // L
+    printf("%x\n", c);  // 4c
+    printf("%X\n", c);  // 4C
+
+    char a = 0x4A;
+    printf("%d\n", a);  // 74
+    printf("%c\n", a);  // J
+    printf("%x\n", a);  // 4a
+    printf("%X\n", a);  // 4A
+
+    int d;
+    scanf("%d", &d);    // 71
+    printf("%c\n", d);  // G
+    int x;
+    scanf("%X", &x);    // 47
+    printf("%c\n", x);  // G
 
     if (argc != 2) {
         printf("Usage: %s <0 for encoding or 1 for decoding>\n", argv[0]);
@@ -23,21 +42,39 @@ int main(int argc, char *argv[]) {
 }
 
 void encode() {
-    printf("Enter characters separeted by a space:\n");
-
-    char ch;
-    char space;
-    // char enter;
-    // int condition = 1;
-
-
-    if (scanf("%c %c", &ch, &space) == 2) {
-      printf("%c %c", ch, space);
-    } else {
-      printf("n/a\n");
+    char c, space;
+    while (1) {
+        scanf("%c%c", &c, &space);
+        if (space == '\n') {
+            printf("%X", c);
+            break;
+        }
+        if (space != ' ') {
+            printf("n/a");
+            break;
+        }
+        printf("%X ", c);
     }
 }
 
 void decode() {
-  printf("Enter two-character strings separated by a space:\n");
+    int x;
+    char c;
+    while (1) {
+        scanf("%X", &x);
+        c = getchar();
+        if (x > 0x7A || x < 0x30) {
+            printf("n/a");
+            break;
+        }
+        if (c == '\n') {
+            printf("%c", x);
+            break;
+        }
+        if (c != ' ') {
+            printf("n/a");
+            break;
+        }
+        printf("%c ", x);
+    }
 }
