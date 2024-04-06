b2303c1a6013b34dd67ce29a4275bd93deb95824
borisweb2020 <49887284+borisweb2020@users.noreply.github.com>
2024-04-06 14:29:41 +0700

    Fix fibonacci calculating with dynamic programming

--- 2_Basic_control_structures/fibonacci.c
+++ 2_Basic_control_structures/fibonacci.c
@@ -1,29 +1,58 @@
 #include <stdio.h>
 
-int fibo(int n);
+int fibo_recursive(int n);
+int fibo_dynamic(int n);
+int fibo_loop(int n);
+
+static int cache[100] = {0};
 
 int main() {
-    printf("%d\n", fibo(10));
+    int a;
+    char c;
 
-    /* ЦИКЛ для вычисления числа Фибоначчи: */
-    int f1 = 1, f2 = 0, x;
-    int n = 10;
-
-    for (int i = 2; i <= n; i++) {
-        x = f1 + f2;
-        f2 = f1;
-        f1 = x;
+    if (scanf("%d%c", &a, &c) == 2 && c == '\n') {
+        if (a < 0) a *= -1;
+        printf("%d", fibo_dynamic(a));
+    } else {
+        printf("n/a");
     }
-
-    printf("%d\n", x);
 
     return 0;
 }
 
-/* ФУНКЦИЯ для вычисления числа Фибоначчи: */
-int fibo(int n) {
+/* Dynamic Programming */
+
+// 1. Top-down approach (recursion with caching):
+
+int fibo_recursive(int n) {
+    if (n <= 1) return n;
+    if (cache[n] == 0) cache[n] = fibo_recursive(n - 1) + fibo_recursive(n - 2);
+
+    return cache[n];
+}
+
+// 2. Bottom-up approach:
+int fibo_dynamic(int n) {
+    int fibo_sequence[n + 1];
+    fibo_sequence[0] = 0;
+    fibo_sequence[1] = 1;
+    for (int i = 2; i <= n; i++) fibo_sequence[i] = fibo_sequence[i - 1] + fibo_sequence[i - 2];
+
+    return fibo_sequence[n];
+}
+
+/* Looping Method */
+int fibo_loop(int n) {
     if (n == 0) return 0;
     if (n == 1) return 1;
+    int f_1 = 1;
+    int f_2 = 1;
+    int result;
+    for (int i = 2; i < n; i++) {
+        result = f_1 + f_2;
+        f_1 = f_2;
+        f_2 = result;
+    }
 
-    return fibo(n - 1) + fibo(n - 2);
+    return result;
 }
